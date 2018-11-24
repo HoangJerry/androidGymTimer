78729ad1cbaf46e83b7bf0c826416c6d2abe2c83
Milano15662 <minh.milano1511@gmail.com>
2018-11-24 13:36:35 +0700

    'locale'

--- app/src/main/java/com/milano/minh/gymtimer/LevelActivity.java
+++ app/src/main/java/com/milano/minh/gymtimer/LevelActivity.java
@@ -1,43 +1,62 @@
 package com.milano.minh.gymtimer;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Locale;
+
 public class LevelActivity extends AppCompatActivity {
-    Button btn_muc1, btn_muc2, btn_muc3;
+    Button btn_muc1, btn_muc2, btn_VN, btn_US;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
-
-        // nhan user tu mh1
-        Bundle bd = getIntent().getExtras();
-        if(bd!=null) {
-
-        }
-
         btn_muc1=(Button) findViewById(R.id.btn_muc1);
         btn_muc1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(LevelActivity.this, LessonActivity.class);
+                startActivity(intent);
             }
         });
         btn_muc2=(Button) findViewById(R.id.btn_muc2);
-        btn_muc1.setOnClickListener(new View.OnClickListener() {
+        btn_muc2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(LevelActivity.this, LessonActivity.class);
+                startActivity(intent);
             }
         });
-        btn_muc3= (Button) findViewById(R.id.btn_muc3);
-        btn_muc3.setOnClickListener(new View.OnClickListener() {
+        btn_VN=(Button) findViewById(R.id.btn_VN);
+        btn_VN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setLanguage("vi");
             }
         });
+        btn_US= (Button) findViewById(R.id.btn_US);
+        btn_US.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLanguage("en");
+            }
+        });
+    }
+
+    private void setLanguage(String language) {
+        Locale locale = new Locale(language);
+        Configuration config = new Configuration();
+        config.locale= locale;
+        getBaseContext().getResources().updateConfiguration(
+                config,
+                getBaseContext().getResources().getDisplayMetrics() );
+        Intent intent = new Intent(LevelActivity.this, LevelActivity.class);
+        startActivity(intent);
+
     }
 }
