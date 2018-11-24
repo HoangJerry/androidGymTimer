aa96c7ee490af3b51e609eba13adf1d5e0a75577
anh1409 <nanh434@gmail.com>
2018-11-24 12:24:44 +0700

    change language

--- app/src/main/java/com/milano/minh/gymtimer/MainActivity.java
+++ app/src/main/java/com/milano/minh/gymtimer/MainActivity.java
@@ -10,17 +10,17 @@ import android.widget.Toast;
 import com.milano.minh.gymtimer.database.Database;
 
 public class MainActivity extends AppCompatActivity {
-    final String DATABASE_NAME = "gymdb.sqlite3";
-    SQLiteDatabase database;
+    // final String DATABASE_NAME = "gymdb.sqlite3";
+    // SQLiteDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        database = Database.initDatabase(this, DATABASE_NAME);
-        Cursor cursor = database.rawQuery("SELECT * FROM exercise", null);
-        cursor.moveToFirst();
-        Toast.makeText(this, cursor.getString(1), Toast.LENGTH_LONG).show();
+        // database = Database.initDatabase(this, DATABASE_NAME);
+        // Cursor cursor = database.rawQuery("SELECT * FROM exercise", null);
+        // cursor.moveToFirst();
+        // Toast.makeText(this, cursor.getString(1), Toast.LENGTH_LONG).show();
         new android.os.Handler().postDelayed(
                 new Runnable() {
                     public void run() {
